00d9775df3065005789a73fa479731623343eeac
unknown <nataliematos12@gmail.com>
2023-04-03 10:22:17 -0400

    added test.js

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,16 +1,18 @@
-const {myStr, data, fruits} = require('../index.js')
+const {myStr, data, fruits} = require('../index.js');
 
-it('Should return my string matching - this is my string', () => {
-    expect(myStr).toBe('this is my string');
-});
-
-it('Should match the object', () => {
-    expect(data).toMatchObject({
-        name: 'JD',
-        age: 43
+describe('Basic Tests', () => {
+    it('Should return my string matching - this is my string', () => {
+        expect(myStr).toBe('this is my string');
     });
-});
 
-it('Should match the array', () => {
-    expect(fruits).toEqual(['orange', 'apple', 'kiwi']);
+    it('Should match the object', () => {
+        expect(data).toMatchObject({
+            name: 'JD',
+            age: 43
+        });
+    });
+
+    it('Should match the array', () => {
+        expect(fruits).toEqual(['orange', 'apple', 'kiwi']);
+    });
 });
